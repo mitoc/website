636da270c1e1f7f2c7211e90e997d38a37698fe4
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-11-16 16:51:58 -0500

    Clean js file

--- js/mitoc.js
+++ js/mitoc.js
@@ -1,32 +1,6 @@
 ---
 ---
 
-//Load leader lists from Google Sheets and populate the table
-function loadLeadersFromSpreadsheet(spreadsheetId, sheetId, divId) {
-    $.getJSON('https://spreadsheets.google.com/feeds/list/' + spreadsheetId + '/' + sheetId
-        + '/public/values?alt=json', function(data) {
-        //Format the data into HTML, with special treatment for category headings
-        var leaderListEntries = '';
-        var leaders = data.feed.entry;
-        var l = leaders.length;
-        for(var i = 0; i < l; i++) {
-            if(leaders[i].gsx$position.$t == 'category') {
-                leaderListEntries = leaderListEntries
-                    + '</div>\n<h3>' + leaders[i].gsx$name.$t + '</h3>\n<div class="row is-flex">\n';
-            } else {
-                leaderListEntries = leaderListEntries
-                    + '<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6"><img height="100" width="100" src="images/leaders/'
-                    + (leaders[i].gsx$photo.$t || 'beaver.jpg') + '"><em>'
-                    + leaders[i].gsx$name.$t + '<br>'
-                    + (leaders[i].gsx$position.$t ? leaders[i].gsx$position.$t + '<br>' : '') + '</em>'
-                    + leaders[i].gsx$description.$t + '</div>\n';
-            }
-        }
-        //Write into div with id="leader-list"
-        $(divId).html(leaderListEntries);
-    });
-}
-
 //Load leader lists from mitoc-trips.mit.edu and populate the table
 function loadLeadersFromTripsWebsite(activityId, divId) {
     $.getJSON('https://mitoc-trips.mit.edu/leaders.json/' + activityId, function(data) {
