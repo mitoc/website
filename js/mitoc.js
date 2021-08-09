9455a6c6722c63786a1490d54825b6a3c47abe08
Cole Crawford <cole_crawford@harvard.edu>
2021-08-09 13:58:52 -0400

    Update mitoc.js
    Gravitar can take an asset as default, loads much faster without checking to see if Gravitar returns a 404 for all items

--- js/mitoc.js
+++ js/mitoc.js
@@ -1,14 +1,6 @@
 ---
 ---
 
-//Helper function to see if a URL returns a 404
-function urlExists(url){
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status!=404;
-}
-
 //Load leader lists from mitoc-trips.mit.edu and populate the table
 function loadLeadersFromTripsWebsite(activityId, divId) {
     $.getJSON('https://mitoc-trips.mit.edu/leaders.json/' + activityId, function(data) {
@@ -17,14 +9,10 @@ function loadLeadersFromTripsWebsite(activityId, divId) {
         var leaders = data.leaders;
         var l = leaders.length;
         for(var i = 0; i < l; i++) {
-		    // Try to get the user's Gravatar; otherwise, use the beaver as a fallback
+		    // Try to get the user's Gravatar; otherwise, use the beaver as a fallback rather than the Gravatar fallback
             var gravUrl = new URL(leaders[i].gravatar);
-            gravUrl.searchParams.set("d", "404"); // return a 404 if not found
-            if(urlExists(gravUrl)){
-                leaders[i].gravatar = gravUrl;
-            } else {
-                leaders[i].gravatar = 'https://mitoc.mit.edu/images/leaders/Beaver.jpg';
-            }
+            gravUrl.searchParams.set("d", "https://mitoc.mit.edu/images/leaders/Beaver.jpg");
+            leaders[i].gravatar = gravUrl.href;
 
             leaderListEntries = leaderListEntries
                     + '<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6"><img height="100" width="100" src="'
